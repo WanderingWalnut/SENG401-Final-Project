b81300c7d133d14b9d129796dad170ce418a9e10
WanderingWalnut <naveed.khan1@ucalgary.ca>
2025-03-21 22:23:55 -0600

    Forcing vercel update

--- backend/reset_db.sql
+++ backend/reset_db.sql
@@ -3,3 +3,7 @@ USE BudgetWise;
 DELETE FROM budget_data;
 -- Reset auto-increment counter
 ALTER TABLE budget_data AUTO_INCREMENT = 1;
+-- Delete all users but keep user accounts
+DELETE FROM users;
+-- Reset auto-increment counter
+ALTER TABLE users AUTO_INCREMENT = 1;
